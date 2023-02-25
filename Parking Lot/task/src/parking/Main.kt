449ffb98b0500ae66f617334b37a2f6a5571202a
My Name <ccrpted@gmail.com>
2023-02-25 14:55:13 +0300

    Stage 4/5: Size matters Description In real life, parking lots vary in size. At this stage, we will get better at making art imitate life. To do this, we will make our program customizable by adding a create command that allows the user to specify the number of spots. For example, the command create 3 makes a parking lot with three spots. The number of spots should be positive. The program output should be the following: Created a parking lot with 3 spots.
    Other commands like park or leave should return an error Sorry, a parking lot has not been created. until the user enters the create command. If the user calls create again, the previous parking state should be reset.
    
    It is also important to keep track of which spaces are occupied by which cars. For this, add a status command that prints all occupied spots in ascending order. For each spot, it should print the spot number, the car’s plate registration number, and the color of the car, all separated by spaces like the example below. If there are no occupied spots, the program should print: Parking lot is empty.

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,35 +1,83 @@
 package parking
 
 fun main() {
-    var spots = MutableList(20) { true }
-
-
+    var parking: MutableList<Boolean> = mutableListOf()
+    var cars: MutableList<String> = mutableListOf()
 
     while (true) {
-
         var data = readln().split(" ")
-        if (data.contains("park")) {
-            if (spots.any { it.equals(true) }) {
-                println("${data[2]} car parked in spot ${spots.indexOf(true) + 1}.")
-                spots[spots.indexOf(true)] = false
 
-            } else if (spots.all { it.equals(false) }) {
-                println("Sorry, the parking lot is full.")
+        if (data.contains("create")) {
+
+            parking.clear()
+            parking = MutableList(data[1].toInt()) { true }
+            cars = MutableList(data[1].toInt()) { "" }
+            println("Created a parking lot with ${data[1]} spots.")
+        } else if (data.contains("status") && parking.all { it.equals(true) } && parking.size > 0) {
+
+            println("Parking lot is empty.")
+        } else if (data.contains("park") && parking.any { it.equals(true) }) {
+            println("${data[2]} car parked in spot ${parking.indexOf(true) + 1}.")
+            var l = parking.indexOf(true)
+            parking[parking.indexOf(true)] = false
+
+            cars[l] = "${l + 1} ${data[1]} ${data[2]}"
+        } else if (data.contains("status") && parking.any { it.equals(false) }) {
+            for (i in cars) {
+                if (i.isNotBlank()) {
+                    println("$i")
+                }
             }
-        } else if (data.contains("leave")) {
+        } else if (data.contains("leave") && parking.any { it.equals(false) }) {
 
-            if (!spots[data[1].toInt() - 1]) {
+            if (!parking[data[1].toInt() - 1]) {
+
 
                 println("Spot ${data[1]} is free.")
-                spots[data[1].toInt() - 1] = true
+                parking[data[1].toInt() - 1] = true
+                cars[data[1].toInt() - 1] = ""
+
 
             }
         } else if (data.contains("exit")) {
             break
+        } else if (parking.all { it.equals(false) } && parking.size > 0) {
+            println("Sorry, the parking lot is full.")
+        } else {
+            println("Sorry, a parking lot has not been created.")
         }
 
-
-
     }
-
 }
+
+
+
+
+
+
+//    while (true) {
+//
+//        var data = readln().split(" ")
+//        if (data.contains("park")) {
+//            if (spots.any { it.equals(true) }) {
+//                println("${data[2]} car parked in spot ${spots.indexOf(true) + 1}.")
+//                spots[spots.indexOf(true)] = false
+//
+//            }
+//        } else if (data.contains("leave")) {
+//
+//            if (!spots[data[1].toInt() - 1]) {
+//
+//                println("Spot ${data[1]} is free.")
+//                spots[data[1].toInt() - 1] = true
+//
+//            }
+//        } else if (data.contains("exit")) {
+//            break
+//        }
+//
+//
+//
+//    }
+
+//}
